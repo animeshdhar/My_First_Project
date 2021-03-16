1cada8ffe0caf311443cb4ed1af4b279d13134a9
Animesh_Dhar <77932603+animeshdhar@users.noreply.github.com>
2021-03-17 00:25:00 +0600

    Delete Thanks_letter.go

--- Thanks_letter.go
+++
@@ -1,8 +0,0 @@
-package main
-
-import "fmt"
-
-func main(){
- fmt.Println("Dear Mostain Billah Sir, Thanks a lot for your kindness for us also i want to thanks our honourable Jafor Raja Chowdhury sir, Registrar of Bangladesh Copyright Office.")
-
-}
